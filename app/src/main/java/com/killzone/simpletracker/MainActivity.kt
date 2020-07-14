95eaa909994f8bf874c8af7eb4e771bccc367aa6
compositeMaterial <elanakov.v@gmail.com>
2020-07-14 22:39:01 +0700

    Third commit

--- app/src/main/java/com/killzone/simpletracker/MainActivity.kt
+++ app/src/main/java/com/killzone/simpletracker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.killzone.simpletracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.killzone.simpletracker.db.TrackerDao
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var dao: TrackerDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        textView.setText("RUNDAO: ${dao.hashCode()}")
+
     }
+
 }
